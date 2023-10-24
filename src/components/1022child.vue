87dd7d4a34b0234a35d93faf644df78579681d97
k29944397 <k29944397@gmail.com>
2023-10-24 17:43:57 +0800

    記帳

---
+++ src/components/1022child.vue
@@ -0,0 +1,35 @@
+<script>
+export default{
+    data(){
+        return{
+            info:[],
+        }
+    },
+    methods:{
+        getInfo(){
+            this.$emit("get",this.info)
+        },
+        show(){
+
+        }
+    }
+}
+</script>
+<template>
+    <div class="c1">    
+        <div class="textArea">
+            
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.c1{
+    display: flexbox;
+    justify-content: center;
+    height: 600px;
+    width: 300px;
+    background-color: aquamarine;
+}
+
+</style>
--- src/views/1022mainPage.vue
+++ src/views/1022mainPage.vue
@@ -1,4 +1,5 @@
 <script>
+import child1022 from "../components/1022child.vue";
 export default{
     date(){
         return{
@@ -6,7 +7,9 @@ export default{
             totalm:0,
             totalpm:totalp+totalm,
             plus:[],
-            minus:[]
+            minus:[],
+            radio:{
+            },
         }
     },
     methods:{
@@ -20,16 +23,29 @@ export default{
                 total2 += minus[x];
             }
         },
-
+        show(){
+            add.style.visibility = "visible";
+        },
+    },
+    components:{
+        child1022
     }
 }
 </script>
 
 <template>
         <div class="add">
-            <input type="text" name="" id="">
-            <input type="number" name="" id="">
-            <button>確定</button>
+            <div class="addmain">
+                <label for="">text</label>
+                <input type="text" name="" id="">
+                <label for="">number</label>
+                <input type="number" name="" id="">
+                <label for="">收入</label>
+                <input type="radio" v-model="radio" name="income" value="plus">
+                <label for="">支出</label>
+                <input type="radio" v-model="radio" name="income" value="minus">
+                <button>確定</button>
+            </div>
         </div>
     <body>
     <div class="leftBody">
@@ -38,23 +54,16 @@ export default{
         <h4>YOUR BALANCE</h4>
         <h5 v-text="'$'+totalpm"></h5>
     </div>
-
     <div class="rightBody">
         <div class="rightBody_Top">
-            <h1 class="t1">INCOME
-                <h1 v-text="'$'+totalp"></h1>
-            </h1>
-            <h2 class="t2">EXPENSE
-                <h2 v-text="'$'+totalm"></h2>
-            </h2>
-        </div>
-        <!-- 預計刪除改成props -->
+            <h1 class="t1">INCOME</h1>
+            <!-- <p>{{ totalp }}</p> -->
+            <h2 class="t2">EXPENSE</h2>
+            <!-- <p>{{ totalm }}</p> -->
+            <button class="addBtn" @click="show()">Add transaction</button>
+        </div> 
         <div class="rightBody_B">
-            <button class="addBtn">Add transaction</button>
-            <div class="ie1">Breakfast $65.00 <button>DL</button><br></div>
-            <div class="ie2">Lunch $90.00<button>DL</button><br></div>
-            <div class="ie3">Design Case $100000.00<button>DL</button><br></div>
-            <div class="ie4">BlueRay Video $1420.00<button>DL</button></div>
+            <child1022 />
         </div>
     </div>
 </body>
@@ -65,13 +74,15 @@ $maincolor: rgb(55,123,170);
 body{
     margin: 0;
     display: flex;
+    width: 600px;
+    height: 100px;
 }
 .leftBody{
     margin: 0;
     background-color: $maincolor;
     color: #FFF;
-    height: 100vh;
-    width: 40vw;
+    height: 1000px;
+    width: 400px;
     h1{
         margin-top: 50px;
         display: flex;
@@ -94,40 +105,70 @@ body{
 }
 .rightBody{
     margin: 0;
+    width: 400px;
+    height: 1000px;
+    position: relative;
+    display: flex;
+    button{
+        top: 150px;
+        position: absolute;
+        background-color:  rgb(55,123,170);
+        color: #fff;
+        border-radius:15%;
+    }
 }
 .rightBody_Top{
+    position: absolute;
     margin: 0;
-    margin-top: 50px;
+    margin-top: 20px;
     background-color: #FFF;
-    height: 30vh;
-    width: 60vw;
+    height: 200px;
+    width: 400px;
     display: flex;
     justify-content:center;
     h1{
         margin: 0;
-        color: rgb(186,198,164);
+        color: rgb(13, 219, 133);
         display: flex;
         font-size: 24pt;
     }
     h2{
         margin-top: 0;
-        color: rgb(138,99,107);;
+        color: rgb(189, 10, 46);;
         display: flex;
         font-size: 24pt;
     }
 }
 .add{
+    visibility:hidden;
+    background-position: center center;
+    background-size: cover;
+    position: absolute;
     height: 100vh;
     width: 100vw;
-    display: none;
-    background-color: rgb(0,0,0, 0.5)
+    background-color: rgb(0,0,0, .25);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+    backdrop-filter: opacity(100%);
+    .addmain{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #FFF;
+        height: 400px;
+        width: 600px;
+        z-index: 1;
+    }
 }
 .rightBody_B{
-    margin-top: 0;
-    padding-left: 20vw;
-    height: 70vh;
-    width: 60vw;
-    background-color: #797979;
+    position: absolute;
+    margin-top: 200px;
+    height: 800px;
+    width: 400px;
+    display: flex;
+    justify-content: center;
     .addBtn{
         background-color: $maincolor;
         color: #FFF;
@@ -141,38 +182,5 @@ body{
         color: #FFF;
         border-radius:10%;
     }
-}
-.ie1{
-    height: 30px;
-    width: 250px;
-    margin: 0;
-    color: $maincolor;
-    border: 1px solid rgb(55,123,170);
-    border-radius:10%;
-    
-}
-.ie2{
-    height: 30px;
-    width: 250px;
-    margin: 0;
-    color: $maincolor;
-    border: 1px solid rgb(55,123,170);
-    border-radius:10%;
-}
-.ie3{
-    height: 30px;
-    width: 250px;
-    margin: 0;
-    color: $maincolor;
-    border: 1px solid rgb(55,123,170);
-    border-radius:10%;
-}
-.ie4{
-    height: 30px;
-    width: 250px;
-    margin: 0;
-    color: $maincolor;
-    border: 1px solid rgb(55,123,170);
-    border-radius:10%;
 }
 </style>
